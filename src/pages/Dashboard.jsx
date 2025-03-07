8db00ba03e40e90eb4398b7f30e3a90fc0432d19
vikramkumar1009 <134327922+vikramkumar1009@users.noreply.github.com>
2025-03-07 16:15:00 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -85,9 +85,9 @@ const Dashboard = () => {
         {/* Logged-in User Details */}
         {loggedInUser && (
           <div className="bg-white p-4 rounded-lg mb-6">
-            <p className="text-lg font-semibold">{loggedInUser.name}</p>
-            <p className="text-sm">{loggedInUser.email}</p>
-            <p className="text-sm capitalize">{loggedInUser.role}</p>
+            <p className="text-lg text-black font-semibold">{loggedInUser.name}</p>
+            <p className="text-sm text-black">{loggedInUser.email}</p>
+            <p className="text-sm text-black capitalize">{loggedInUser.role}</p>
           </div>
         )}
 
